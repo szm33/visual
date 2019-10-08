969ce2954e88a89e8590f425ae74e5509df980eb
Szymon Dobrowolski <216747@edu.p.lodz.pl>
2019-10-08 10:58:53 +0200

    dodanie klasy katalog

---
+++ appVisual/appVisual/Katalog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appVisual
+{
+    class Katalog
+    {
+        private string tytul;
+        private string opis;
+        private int id;
+
+        public string Tytul
+        {
+            get { return tytul; }
+            set { tytul = value; }
+        }
+        public string Opis
+        {
+            get { return opis; }
+            set { opis = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
