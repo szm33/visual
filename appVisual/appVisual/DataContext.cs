80f4b9613b3ccdde1b7363d34c25bd4e0f3b917f
Szymon Dobrowolski <216747@edu.p.lodz.pl>
2019-10-09 13:44:06 +0200

    Wstępna część 1

---
+++ appVisual/appVisual/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace appVisual
+{
+    class DataContext
+    {
+        public List<Wykaz> czytelnicy;
+        public Dictionary<int, Katalog> ksiazki;
+        public ObservableCollection<Zdarzenie> zdarzenia;
+        public List<OpisStanu> opisy_stanow;
+
+        public DataContext(List<Wykaz> czytelnicy, Dictionary<int, Katalog> ksiazki, ObservableCollection<Zdarzenie> zdarzenia, List<OpisStanu> opisy_stanow)
+        {
+            this.czytelnicy = czytelnicy;
+            this.ksiazki = ksiazki;
+            this.zdarzenia = zdarzenia;
+            this.opisy_stanow = opisy_stanow;
+        }
+    }
+}
