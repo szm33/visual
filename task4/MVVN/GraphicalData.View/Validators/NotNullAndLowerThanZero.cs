03af4969e64466e7cc33e943390ce70733e84767
MaciejBartos <maciejbartos@outlook.com>
2020-01-22 17:39:34 +0100

    dodanie ValidationRules do WPF

---
+++ task4/MVVN/GraphicalData.View/Validators/NotNullAndLowerThanZero.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace GraphicalData.View.Validators
+{
+    public class NotNullAndLowerThanZero : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (int.TryParse(value.ToString(), out int i))
+            {
+                if (((int)value) <= 0)
+                {
+                    return new ValidationResult(false, "ID cannot be lower than or equal to 0");
+                }
+                if (((int?)value) == null)
+                {
+                    return new ValidationResult(false, "ID cannot be null!");
+                }
+                return new ValidationResult(true, null);
+            }
+            return new ValidationResult(false, "ID has to be a number!");
+
+
+        }
+    }
+}
